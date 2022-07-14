ff729b2036cf632590abcf87b6e5b8af400d5712
Raymond Reggers <raymond@adaptiv.nl>
2022-07-14 11:49:00 +0200

    fix compatibility

--- erpnext/subcontracting/doctype/subcontracting_receipt/subcontracting_receipt.js
+++ erpnext/subcontracting/doctype/subcontracting_receipt/subcontracting_receipt.js
@@ -53,18 +53,18 @@ frappe.ui.form.on('Subcontracting Receipt', {
 	refresh: (frm) => {
 		if (frm.doc.docstatus > 0) {
 			frm.add_custom_button(__("Stock Ledger"), function () {
-				frappe.route_options = {
+				const route_options = {
 					voucher_no: frm.doc.name,
 					from_date: frm.doc.posting_date,
 					to_date: moment(frm.doc.modified).format('YYYY-MM-DD'),
 					company: frm.doc.company,
 					show_cancelled_entries: frm.doc.docstatus === 2
 				};
-				frappe.set_route("query-report", "Stock Ledger");
+				frappe.set_route("query-report", "Stock Ledger", route_options);
 			}, __("View"));
 
 			frm.add_custom_button(__('Accounting Ledger'), function () {
-				frappe.route_options = {
+				const route_options = {
 					voucher_no: frm.doc.name,
 					from_date: frm.doc.posting_date,
 					to_date: moment(frm.doc.modified).format('YYYY-MM-DD'),
@@ -72,7 +72,7 @@ frappe.ui.form.on('Subcontracting Receipt', {
 					group_by: "Group by Voucher (Consolidated)",
 					show_cancelled_entries: frm.doc.docstatus === 2
 				};
-				frappe.set_route("query-report", "General Ledger");
+				frappe.set_route("query-report", "General Ledger", route_options);
 			}, __("View"));
 		}
 
