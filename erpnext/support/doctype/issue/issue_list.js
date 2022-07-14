af12cf702865c477187da2ab911d5361f24050cf
Raymond Reggers <raymond@adaptiv.nl>
2022-07-14 12:16:32 +0200

    fix issue list filters

--- erpnext/support/doctype/issue/issue_list.js
+++ erpnext/support/doctype/issue/issue_list.js
@@ -1,7 +1,7 @@
 frappe.listview_settings['Issue'] = {
 	colwidths: {"subject": 6},
 	add_fields: ['priority'],
-	filters = ["status", "=", "open"],
+	filters: [["status", "=", "open"]],
 
 	onload: function(listview) {
 		var method = "erpnext.support.doctype.issue.issue.set_multiple_status";
